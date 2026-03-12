0cdbf13c2cd400b601f8740c888818c6e0a064d9
koo111333 <koo111333@gmail.com>
2026-03-12 14:29:17 +0000

    fix: Update Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "paslytics",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDFwF4kN4LCNf5y2JLvaW9P5l9WKMk0noY",
+  authDomain: "paslytics-80711756-8913a.firebaseapp.com",
+  projectId: "paslytics-80711756-8913a",
+  storageBucket: "paslytics-80711756-8913a.firebasestorage.app",
+  messagingSenderId: "278824874614",
+  appId: "1:278824874614:web:8ecfe8587c0ad72f2ae557"
 };
 
 // Initialize Firebase
